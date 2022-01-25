f427040db1c035849d9b45486e6f48bc804ac42e
dishitavashi-sudo <dishitavashi@gmail.com>
2022-01-24 21:59:59 -0600

    c1m1 Final submission

--- stats.c
+++ stats.c
@@ -39,33 +39,106 @@ void main() {
   /* Other Variable Declarations Go Here */
   /* Statistics and Printing Functions Go Here */
 
+  unsigned char min = find_minimum(test, SIZE);
+  unsigned char max = find_maximum(test, SIZE);
+  float mean = find_mean(test, SIZE);
+  float median = find_median(test, SIZE);
+  print_statistics(mean, median, min, max);
+  printf("Sorted array : \n");
+  sort_array(test, SIZE);  
+  print_array(test, SIZE);
 }
 
 /* Add other Implementation File Code Here */
 void print_statistics(float mean, float median, unsigned char min, unsigned char max) {
-
+	printf("Minimum is %u\n", min);
+	printf("Maximum is %u\n", max);
+	printf("Mean is %f\n", mean);
+	printf("Median is %f\n", median);
+	return;
 }
 
 void print_array(unsigned char *ptr, unsigned int size){
+	int i;
+
+	for(i = 0; i < size; i++) {
+		printf("%u \t", ptr[i]);	
+	}
+	return;
+}
+
+static void sort_array_ascending(unsigned char *ptr, unsigned int size){
+	unsigned char temp;
+	int i, j;
+
+	for(i = 0; i < size - 2; i++) {
+		for(j = 0; j < size - 1; j++) { 
+			if(ptr[j] > ptr[j+1]) {
+				temp = ptr[j];
+				ptr[j] = ptr[j+1];
+				ptr[j+1] = temp;			
+			}		
+		}	
+	}
+	return;
 
 }
 
+/* This function calculates the median of all members of the given array */
 float find_median(unsigned char *ptr, unsigned int size){
-
+	
+	sort_array_ascending(ptr, size);
+	if(size % 2 == 0) {
+		return (ptr[size/2] + ptr[(size/2) - 1]) / 2;	
+	}
+	else
+		return ptr[size/2];
 }
 
+/* This function calculates the mean of all members of the given array */
 float find_mean(unsigned char *ptr, unsigned int size){
+	float sum = 0.0;
+	int i;
 
+	for(i = 0; i < size; i++)
+		sum += ptr[i];
+
+	return sum / size;
 }
 
 unsigned char find_minimum(unsigned char *ptr, unsigned int size){
+	unsigned char min = ptr[0];
+	int i;
 
+	for(i = 1; i < size; i++) {
+		if(ptr[i] < min)
+			min = ptr[i];	
+	}
+	return min;
 }
 
 unsigned char find_maximum(unsigned char *ptr, unsigned int size){
-
+	unsigned char max = ptr[0];
+	int i;
+	
+	for(i = 1; i < size; i++) {
+		if(ptr[i] > max)
+			max = ptr[i];	
+	}
+	return max;
 }
 
 void sort_array(unsigned char *ptr, unsigned int size){
+	sort_array_ascending(ptr, size);
+	unsigned int size_var = size;
+	int i;
+	unsigned char temp;
 
+	for(i = 0; i < (size/2) - 1; i++) {
+		temp = ptr[i];
+		ptr[i] = ptr[size_var - 1];
+		ptr[size_var - 1] = temp;
+		size_var--;
+	}
+	return;
 }
